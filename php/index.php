87a3e70a0bdb07c1195974e5cbd162d6e9f69bea
Курушин Даниил <daniel.kurushin@gmail.com>
2017-09-09 13:06:28 +0500

    gui

--- php/index.php
+++ php/index.php
@@ -1,18 +1,25 @@
 <html>
 <head>
 	<style>
-		.button {
-			background-color: darkblue;
-		}
+	.button {
+		background-color: #00558b;
+		margin: 10pt;
+		padding: 10pt;
+		border-radius: 10pt;
+	}
+
+	.button a {
+		color: white;
+	}
 	</style>
 </head>
 <body>
 <!--<div class="button">0) проекты - ники хронометражистов</div>-->
-<div class="button">1) операции</div>
-<div class="button">2) должности</div>
-<div class="button">3) подразделения</div>
-<div class="button">4) факторы</div>
-<div class="button">5) хронометражисты</div>
-<div class="button">6) отчет </div>
+<div class="button"><a href='report.php?mode=oper'>Операции</a></div>
+<div class="button"><a href='report.php?mode=dolg'>Должности</a></div>
+<div class="button"><a href='report.php?mode=podr'>Подразделения</a></div>
+<div class="button"><a href='report.php?mode=fact'>Факторы</a></div>
+<div class="button"><a href='report.php?mode=xpoh'>Хронометражисты</a></div>
+<div class="button"><a href='report.php'>Отчет</a></div>
 </body>
 </html>
--- php/report.php
+++ php/report.php
@@ -1,49 +1,91 @@
 <?php
 $dbconn = pg_connect("host=localhost dbname=xponometra user=xponometrax password=gfhffvbylb'nbkfybkbycekmafn")
     or die('Could not connect: ' . pg_last_error());
-$result = pg_query("
-select
-	frvh.date_time as date_time,
-	frvh.n_head as n_head,
-	(select name from podr where id = frvh.id_podr) as podr,
-	(select name from dolg where id = frvh.id_dolg) as dolg,
-	(select fio from xpoh where id = frvh.id_xpoh) as xpoh,
-	frvh.sm_nom as sm_nom,
-	frvh.sm_dlit as sm_dlit,
-	frvh.rab_gr_a as rab_gr_a,
-	frvh.rab_gr_b as rab_gr_b,
-	frvh.num_men as num_men,
-	frvh.worker_nik as worker_nik,
-	frvb.time as time,
-	frvb.P3B_OTL_OP as P3B_OTL_OP,
-	frvb.KO as KO,
-	frvb.KP as KP,
-	(select name from oper where id = frvb.oper_id) as oper,
-	(select name from fact where id = frvb.fact1_id) as fact1,
-	(select name from fact where id = frvb.fact2_id) as fact2,
-	(select name from fact where id = frvb.fact3_id) as fact3,
-	frvb.comments as comments
-from frvb, frvh
-where frvb.frvh_id = frvh.id
-order by frvh.date_time;
-");
 
-$rows  = array('date_time', 'n_head', 'podr', 'dolg', 'xpoh', 'sm_nom', 'sm_dlit', 'rab_gr_a', 'rab_gr_b', 'num_men', 'worker_nik', 'time', 'p3b_otl_op', 'ko', 'kp', 'oper', 'fact1', 'fact2', 'fact3', 'comments');
-$rrows = array('Дата/время', 'Номер', 'Подразделение', 'Должность', 'Хронометражист', 'Номер смены', 'Длительность сменя', 'Сутки', 'Через', 'Номер чел', 'Работник', 'ВНО', 'П3B/ОТЛ/ОП', 'KO', 'KП', 'Операция', 'Фактор', 'Фактор', 'Фактор', 'Прим');
-echo "<table border><tr>";
-foreach ($rrows as $v)
+function print_frv()
 {
-	echo "<th>$v</th>";
-}
-echo "</tr>\n";
-while ($row = pg_fetch_assoc($result))
-{
-	echo "<tr>";
-	foreach ($rows as $v)
+	$result = pg_query("
+	select
+		frvh.date_time as date_time,
+		frvh.n_head as n_head,
+		(select name from podr where id = frvh.id_podr) as podr,
+		(select name from dolg where id = frvh.id_dolg) as dolg,
+		(select fio from xpoh where id = frvh.id_xpoh) as xpoh,
+		frvh.sm_nom as sm_nom,
+		frvh.sm_dlit as sm_dlit,
+		frvh.rab_gr_a as rab_gr_a,
+		frvh.rab_gr_b as rab_gr_b,
+		frvh.num_men as num_men,
+		frvh.worker_nik as worker_nik,
+		frvb.time as time,
+		frvb.P3B_OTL_OP as P3B_OTL_OP,
+		frvb.KO as KO,
+		frvb.KP as KP,
+		(select name from oper where id = frvb.oper_id) as oper,
+		(select name from fact where id = frvb.fact1_id) as fact1,
+		(select name from fact where id = frvb.fact2_id) as fact2,
+		(select name from fact where id = frvb.fact3_id) as fact3,
+		frvb.comments as comments
+	from frvb, frvh
+	where frvb.frvh_id = frvh.id
+	order by frvh.date_time;
+	");
+
+	$rows  = array('date_time', 'n_head', 'podr', 'dolg', 'xpoh', 'sm_nom', 'sm_dlit', 'rab_gr_a', 'rab_gr_b', 'num_men', 'worker_nik', 'time', 'p3b_otl_op', 'ko', 'kp', 'oper', 'fact1', 'fact2', 'fact3', 'comments');
+	$rrows = array('Дата/время', 'Номер', 'Подразделение', 'Должность', 'Хронометражист', 'Номер смены', 'Длительность сменя', 'Сутки', 'Через', 'Номер чел', 'Работник', 'ВНО', 'П3B/ОТЛ/ОП', 'KO', 'KП', 'Операция', 'Фактор', 'Фактор', 'Фактор', 'Прим');
+	echo "<table border><tr>";
+	foreach ($rrows as $v)
 	{
-		echo "<td>$row[$v]</td>";
+		echo "<th>$v</th>";
 	}
 	echo "</tr>\n";
+	while ($row = pg_fetch_assoc($result))
+	{
+		echo "<tr>";
+		foreach ($rows as $v)
+		{
+			echo "<td>$row[$v]</td>";
+		}
+		echo "</tr>\n";
+	}
+	echo "</table>";
 }
-echo "</table>";
+
+function print_table($table, $order)
+{
+	$result = pg_query("select * from $table order by $order;");
+	$is_first_row = true;
+	echo "<table border>";
+	while ($row = pg_fetch_assoc($result))
+	{
+		if($is_first_row)
+		{
+			echo "<tr>";
+			foreach (array_keys($row) as $v)
+			{
+				echo "<th>$v</th>";
+			}
+			echo "</tr>\n";
+			$is_first_row = false;
+		}
+		echo "<tr>";
+		foreach ($row as $v)
+		{
+			echo "<td>$v</td>";
+		}
+		echo "</tr>\n";
+	}
+	echo "</table>";
+}
+
+if (isset($_GET['mode']))
+{
+	if ($_GET['mode'] == "oper") print_table("oper","name");
+	if ($_GET['mode'] == "dolg") print_table("dolg","name");
+	if ($_GET['mode'] == "podr") print_table("podr","name");
+	if ($_GET['mode'] == "fact") print_table("fact","name");
+	if ($_GET['mode'] == "xpoh") print_table("xpoh","fio");
+} else {
+	print_frv();
+}
 ?>
